2e5d90e750e2ca93bd107b4ac5b375f13be23289
rlodhiya <rlodhiya2502@gmail.com>
2025-04-08 16:27:23 +0100

    add routing configuration for main application pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { LearnersComponent } from './pages/learners/learners.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { FinanceComponent } from './pages/finance/finance.component';
+import { MoreComponent } from './pages/more/more.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'calendar', component: CalendarComponent },
+    { path: 'learners', component: LearnersComponent },
+    { path: 'jobs', component: JobsComponent },
+    { path: 'finance', component: FinanceComponent },
+    { path: 'more', component: MoreComponent },
+];
