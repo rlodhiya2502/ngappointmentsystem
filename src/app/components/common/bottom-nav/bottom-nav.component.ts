47ccd67fc6b583a8d50653c90050e30d13955d8d
rlodhiya <rlodhiya2502@gmail.com>
2025-04-11 18:15:14 +0100

    add bottom-nav and header components with initial templates, styles, and tests

---
+++ src/app/components/common/bottom-nav/bottom-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-nav',
+  imports: [],
+  templateUrl: './bottom-nav.component.html',
+  styleUrl: './bottom-nav.component.scss'
+})
+export class BottomNavComponent {
+
+}
